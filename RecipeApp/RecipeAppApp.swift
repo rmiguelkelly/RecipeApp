b6b834721f54c7a038e7718908af472c367cde97
Ronan Kelly <30551527+rmiguelkelly@users.noreply.github.com>
2024-12-12 01:28:14 -0500

    Initial Commit

---
+++ RecipeApp/RecipeAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  RecipeAppApp.swift
+//  RecipeApp
+//
+//  Created by Ronan Kelly on 12/12/24.
+//
+
+import SwiftUI
+
+@main
+struct RecipeAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
